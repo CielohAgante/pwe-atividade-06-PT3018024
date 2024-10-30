9193b730483349b44807e1a66d367ffae5d1a923
CielohAgante <160742761+CielohAgante@users.noreply.github.com>
2024-10-30 10:13:37 -0300

    zargo

--- questao3.js
+++ questao3.js
@@ -1,11 +1,12 @@
 let nome = "Marcelo Agante";
 let nota1 = 7;
-let nota2 = 8.5;
-let multnota1 = nota1 * 0.6;
-let multnota2 = nota2 * 0.4;
-let somaNotas = multnota1 + multnota2;
+let nota2 = 8;
 
-alert("Nome do aluno: " + nome);
-alert("Nota 1: " + nota1);
-alert("Nota 2: " + nota2);
-alert("Nota total: " + somaNotas);
+let multNota1 = nota1 * 0.6;
+let multNota2 = nota2 * 0.4;
+let somaNotas = multNota1 + multNota2;
+
+alert("Nome do aluno: " + nome)
+alert("Nota 1: " + nota1)
+alert("Nota 2: " + nota2)
+alert("Nota total: " + somaNotas)
