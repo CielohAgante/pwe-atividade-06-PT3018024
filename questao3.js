ec1fabe869fb89d48882a9e12d4b386b1129fc45
CielohAgante <160742761+CielohAgante@users.noreply.github.com>
2024-10-30 09:40:51 -0300

    zorba

---
+++ questao3.js
@@ -0,0 +1,11 @@
+let nome = "Marcelo Agante";
+let nota1 = 7;
+let nota2 = 8.5;
+let multnota1 = nota1 * 0.6;
+let multnota2 = nota2 * 0.4;
+let somaNotas = multnota1 + multnota2;
+
+alert("Nome do aluno: " + nome);
+alert("Nota 1: " + nota1);
+alert("Nota 2: " + nota2);
+alert("Nota total: " + somaNotas);
